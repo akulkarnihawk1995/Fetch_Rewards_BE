c686ebfbb384cb3739e205534b7248c23a4b3d58
akshay <akulkarni10@hawk.iit.edu>
2021-02-02 19:34:46 -0600

    Add api_points skelton and global data structure

--- routes/api_points.js
+++ routes/api_points.js
@@ -16,6 +16,6 @@ exports.addPoints = (request,response) =>{
 exports.deductPoints = () =>{
 
 }
-exports.balancePoints = (request, response) => {
+exports.balancePoints = (request, response) =>  {
 
 }
