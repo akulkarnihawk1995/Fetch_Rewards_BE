2102242fb086397806047729def26f0cbfb8498f
akshay <akulkarni10@hawk.iit.edu>
2021-02-02 19:17:16 -0600

    Add api route function

---
+++ routes/api_points.js
@@ -0,0 +1,15 @@
+const { response } = require("express");
+/**
+ * 
+ * @param {*} request request object
+ * @param {*} response response object
+ */
+exports.addPoints = (request,response) =>{
+}
+
+exports.deductPoints = () =>{
+
+}
+exports.balancePoints = (request, response) => {
+
+}
