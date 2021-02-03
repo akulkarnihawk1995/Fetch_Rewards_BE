261911864ca37bea60ae9bab1785ca41473c7f0e
akshay <akulkarni10@hawk.iit.edu>
2021-02-02 22:50:18 -0600

    Add helper function

--- utils/helper_func.js
+++ utils/helper_func.js
@@ -8,11 +8,25 @@
 const printUserStore = (userData_Map) => {
     for (let [key, value] of userData_Map.entries()) {
       console.log("Key: " , key);
-      
+
       console.log(value);
     }
   };
-
+const checkBalance = (points_Map, deductionAmount) => {
+  let total = 0;
+  for (let [key, value] of points_Map.entries()) total = total + value;
+  return total >= deductionAmount;
+};
+const sendDeduction = (deductionMap) => {
+    let responseList = [];
+    for (let [key, value] of deductionMap.entries()) {
+      responseList.push({ [key]: value });
+    }
+    return responseList;
+  };
+  
   module.exports = {
-    printUserStore
+    printUserStore,
+    checkBalance,
+    sendDeduction
   };
