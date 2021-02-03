b563a6ec5d8cb5d89c12997f8214de947ec0f5be
akshay <akulkarni10@hawk.iit.edu>
2021-02-02 21:27:05 -0600

    Add api endpoint to server.js

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const { app, port } = require("./utils/middleware")
 
+const { addPoints, deductPoints, balancePoints } = require('./routes/api_points');
+
+
+// add new transaction 
+app.post("/api/add", addPoints);
+
+// deduct point from balance 
+app.post("/api/deduct", deductPoints);
+
+// get balance according to username
+app.get("/api/balance/:username", balancePoints);
 
 //starting the server on the port 5000
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-  });
+});
